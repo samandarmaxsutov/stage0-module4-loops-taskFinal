32638e779eab832b9d368dcde8b8037ced76e195
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-10 19:40:27 +0500

    finished

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int n=1;
+        for (int i = 0; i < power; i++) {
+           n=n*numberToPrint;
+        }
+        System.out.println(n);
 
     }
 
